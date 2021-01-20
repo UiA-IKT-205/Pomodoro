ab55ffe23ad92b30e0e5b62db51fcf3934de0e55
Christian Simonsen <crsdev@gmail.com>
2021-01-20 10:52:13 +0100

    Ferdig nedteling med varsling

--- app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -2,10 +2,51 @@ package no.uia.ikt205.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timer:CountDownTimer
+    lateinit var startButton:Button
+    lateinit var coutdownDisplay:TextView
+
+    val timeToCountDownInMs = 5000L
+    val timeTicks = 1000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       startButton = findViewById<Button>(R.id.startCountdownButton)
+       startButton.setOnClickListener(){
+           startCountDown(it)
+       }
+       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+
     }
+
+    fun startCountDown(v: View){
+
+        timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
+            override fun onFinish() {
+                Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+               updateCountDownDisplay(millisUntilFinished)
+            }
+        }
+
+        timer.start()
+    }
+
+    fun updateCountDownDisplay(timeInMs:Long){
+        coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+    }
+
 }
